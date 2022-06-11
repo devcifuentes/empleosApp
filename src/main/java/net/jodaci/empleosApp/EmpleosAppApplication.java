e506bcec88754f405be395196fec3176523da79e
Jodaci <joda.cifusa@hotmail.com>
2022-06-10 23:48:23 -0500

    Initial Spring Boot

---
+++ src/main/java/net/jodaci/empleosApp/EmpleosAppApplication.java
@@ -0,0 +1,13 @@
+package net.jodaci.empleosApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpleosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpleosAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/jodaci/empleosApp/EmpleosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jodaci.empleosApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
